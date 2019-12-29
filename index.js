8daafc0182b51b3e9a2d5f4a57738e9a4bd22d6b
Prabakaran <prabakaran@Prabakarans-MacBook-Pro.local>
2019-12-29 22:40:49 +0100

    Handling user creation after oauth

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-require('./services/passport');
-
+const keys = require('./config/dev');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+  auth: {
+    user: 'admin',
+    password: '4XG!iM%D5%gT'
+  },
+  useNewUrlParser: true
+}, function(err, client) {
+  if (err) console.log(err);
+  console.log('Db connected!!!');
+});
+
 require('./routes/authentication')(app);
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,22 @@
 const passport = require('passport');
 const keys = require('../config/dev');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
 	clientSecret: keys.googleClientSecret,
 	callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-	console.log('profile', profile);
+	User.findOne({ googleId: profile.id })
+		.then((existingUser) => {
+			if(existingUser) {
+				done(null, existingUser);
+			} else {
+				new User({ googleId: profile.id })
+					.save()
+					.then((user) => done(null, user));
+			}
+		});
 }));
