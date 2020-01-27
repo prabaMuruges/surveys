fa45bfbf7fe4e5b939fdf2668a966bbf8bd76f5a
prabaMuruges <mpraba2020@gmail.com>
2020-01-27 21:45:30 +0100

    Fixing HTTP proxy in client

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Know what your customers think. Sign in to connect better.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/auth/google">Sign in via Google</a>
       </header>
     </div>
   );
