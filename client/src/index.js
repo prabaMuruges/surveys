c726e1044ae25db6cb19526fe03e69089b9610b4
prabaMuruges <mpraba2020@gmail.com>
2020-02-01 00:01:13 +0100

    Introducing provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
+import { CreateStore, applyMiddleWare } from 'react';
+
+const store = CreateStore(() => [], applyMiddleWare());
 
 ReactDom.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.querySelector("#root")
 );
