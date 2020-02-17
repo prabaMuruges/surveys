0a3928b51ed2304e8b00854907327922e1006019
prabaMuruges <mpraba2020@gmail.com>
2020-02-17 23:05:05 +0100

    Handling redirection based on user login

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,6 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {
 	dispatch({
 		type: FETCH_USER,
-		payload: await axios.get('/api/current_user')
+		payload: await (await axios.get('/api/current_user')).data
 	});
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderLoginContent() {
+		let userData = this.props.auth;
+		switch(userData) {
+			case null:
+				return;
+			case false:
+				return(
+					<li><a href="/auth/google">Login with Google</a></li>
+				);
+			default:
+				return(
+					<li><a href="/api/logout">Logout</a></li>
+				);
+		}
+	}
+
   render() {
+		console.log(this.props.auth);
     return(
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">FeedbackTime</a>
           <ul className="right">
-            <li><a href="">Login With Google</a></li>
+						<li>{this.renderLoginContent()}</li>
           </ul>
         </div>
       </nav>
@@ -15,4 +33,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+	return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(Header);
